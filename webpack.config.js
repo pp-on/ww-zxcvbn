9ce3935c7b8e724f7cd5a2614fdd5a4b4b5180d9
Ossi <oswaldo.nickel@gmail.com>
2024-05-04 18:06:33 +0200

    t: add webpack

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/zxcvbn.ts',
+    output: {
+        filename: 'ww-zxcvbn.js', // Output filename
+        path: path.resolve(__dirname, 'dist'), // Output directory path
+    },
+    resolve: {
+        extensions: ['.ts', '.js'],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+        ],
+    },
+};
